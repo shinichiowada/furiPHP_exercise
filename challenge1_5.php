a6a764a476cf0243c857da9fa82c460fec927335
shinichi <0503baku@gmail.com>
2021-03-13 09:57:48 +0900

    課題5の空行削除修正

--- challenge1_5.php
+++ challenge1_5.php
@@ -1,7 +1,6 @@
 <?php
 
 $hp = 10000;
-$attacks = [1 => "剣", 2 => "魔法", 3 => "打撃"];
 
 while ($hp > 0) {
     echo "攻撃技は？" . PHP_EOL . "1.剣 2.魔法 3.打撃" . PHP_EOL;
@@ -16,7 +15,6 @@ while ($hp > 0) {
             if ($damage >= 2000) {
                 echo 'クリティカルヒット！！' . PHP_EOL;
             }
-
             if ($hp - $damage > 0) {
                 $hp -= $damage;
             } else {
@@ -25,11 +23,8 @@ while ($hp > 0) {
             echo '攻撃力' . $damage . 'の攻撃！' . PHP_EOL;
             echo 'HP: ' . $hp . PHP_EOL;
             break;
-
-
         default:
             echo '攻撃に失敗！' . PHP_EOL;
     }
 }
-
 echo '敵を倒した!!' . PHP_EOL;
